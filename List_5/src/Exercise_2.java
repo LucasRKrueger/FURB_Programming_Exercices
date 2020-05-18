b904dfe21be65bb5c23f52af66ecc54a847f2410
LucasRKrueger <lucasrkthd@gmail.com>
2020-05-18 20:48:54 -0300

    Exercise 2

---
+++ List_5/src/Exercise_2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Exercise_2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int numbers = 100;
+		int sumOdd = 0, sumEven = 0;
+		
+		for(int i = 0; i < numbers; i++){
+			if(i % 2 == 0)
+				sumEven += i;
+			else
+				sumOdd += i;
+		}
+		System.out.println("Total Odd: " + sumOdd + " \nTotal Even: " + sumEven);
+			
+		scanner.close();
+	}
+}
